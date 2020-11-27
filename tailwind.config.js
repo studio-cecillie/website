edaee78e922ca4591203046f9b226eb521f31222
Arnaud Ligny <arnaud@ligny.org>
2020-11-27 23:55:02 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
   purge: {
-    enabled: false,
     mode: 'all',
     content: [
       './layouts/**/*.html.twig',
     ]
   },
   theme: {
-    /*fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
-    },*/
     extend: {
       colors: {
         'saumon': '#FCF0EC',
